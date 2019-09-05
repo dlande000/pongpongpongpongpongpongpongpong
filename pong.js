7f035578737ff20e34ecbb1ca6f96c494380ea3d
David Anderson <dlande000@gmail.com>
2019-09-05 12:29:41 -0400

    Add styling

--- pong.js
+++ pong.js
@@ -72,7 +72,7 @@ class Pong {
     }
 
     draw() {
-        this._context.fillStyle = 'rgba(0, 0, 0, 0.1)';
+        this._context.fillStyle = 'rgba(0, 0, 0, 0.08)';
         this._context.fillRect(0, 0, this._canvas.width, this._canvas.height);
         this.balls.forEach((ball, i) => {
             this.drawBall(ball, this.colors[i]);
@@ -103,15 +103,16 @@ class Pong {
         const i = this.balls.indexOf(ball);
         this.balls[i].pos.x = this._canvas.width/2;
         this.balls[i].pos.y = this._canvas.height/2;
-        this.balls[i].vel.x = (((Math.random() * (200)) + 1)) * (Math.random() > 0.5 ? 1 : -1);
-        this.balls[i].vel.y = (((Math.random() * (200)) + 1)) * (Math.random() > 0.5 ? 1 : -1);
+        this.balls[i].vel.x = (((Math.random() * 200) + 1)) * (Math.random() > 0.5 ? 1 : -1);
+        this.balls[i].vel.y = (((Math.random() * 200) + 1)) * (Math.random() > 0.5 ? 1 : -1);
     }
 
     start() {
+        this.players.forEach(player => player.score = 0);
         this.balls.forEach(ball => {
             if (ball.vel.x === 0 || ball.vel.y === 0) {
-                ball.vel.x = (((Math.random() * (200)) + 1)) * (Math.random() > 0.5 ? 1 : -1);
-                ball.vel.y = (((Math.random() * (200)) + 1)) * (Math.random() > 0.5 ? 1 : -1);
+                ball.vel.x = (((Math.random() * 200) + 1)) * (Math.random() > 0.5 ? 1 : -1);
+                ball.vel.y = (((Math.random() * 200) + 1)) * (Math.random() > 0.5 ? 1 : -1);
             }
         });
     }
@@ -136,7 +137,7 @@ class Pong {
             }
             this.players.forEach(player => {
                 this.collide(player, ball);
-                if (player.score > 50) {
+                if (player.score > 49) {
                     this.resetAllBalls();
                 }
             });
