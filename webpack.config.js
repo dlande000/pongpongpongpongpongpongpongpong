062d8881e0d2911ffa7de0995263176129d04427
David Anderson <dlande000@gmail.com>
2019-10-02 16:56:39 -0400

    Remove comments

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,3 @@
-
 const path = require('path');
 
 module.exports = {
@@ -8,20 +7,6 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js'
   },
-//   module: {
-//     rules: [
-//       {
-//         test: /\.jsx?$/,
-//         exclude: /(node_modules)/,
-//         use: {
-//           loader: 'babel-loader',
-//           query: {
-//             presets: ['@babel/env']
-//           }
-//         },
-//       }
-//     ]
-//   },
   devtool: 'source-map',
   resolve: {
     extensions: [".js", "*"]
